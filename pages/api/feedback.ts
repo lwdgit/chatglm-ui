79499963e8dd5277c778034d68f454db4dfafbc9
wenli.lw <wenli.lw@alibaba-inc.com>
2023-04-06 21:08:46 +0800

    feat: 增加日志

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     const { chatid, type = 'good' } = req.query;
+    console.log('chatid', chatid, type);
     if (['good', 'bad'].includes(type as string)) {
       fs.promises.rename(`./prompts/${chatid}.json`, `./prompts/${type}_${chatid}.json`);
     }
